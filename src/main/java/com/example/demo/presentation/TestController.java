d07c4df4fdc536e7a6390f11c8b33abc0d471fa4
JoujiYamanaka <jouji_yamanaka@evolveit.jp>
2026-04-17 19:52:53 +0900

    gitignoreをコミット

---
+++ src/main/java/com/example/demo/presentation/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    @GetMapping(value = "/response", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, String>> getTest(){
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, JSON!");
+        return ResponseEntity.ok(response);
+    }
+}
